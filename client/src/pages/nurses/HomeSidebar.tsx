4b1a0c734930fa6c9713d7c93a15ebdd35f721ea
Arun Prasad <aruntd008@gmail.com>
2024-11-11 16:16:31 +0530

    .

--- client/src/pages/nurses/HomeSidebar.tsx
+++ client/src/pages/nurses/HomeSidebar.tsx
@@ -13,7 +13,6 @@ import {
   LayoutDashboard
 } from "lucide-react"
 
-
 import { NavFavorites } from "@/components/nav-favorites"
 import { NavMain } from "@/components/nav-main"
 import { NavSecondary } from "@/components/nav-secondary"
